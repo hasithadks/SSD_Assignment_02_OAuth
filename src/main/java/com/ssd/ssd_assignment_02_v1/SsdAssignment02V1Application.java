fa8e1f16866bb982689d4996bfe5d723f54a1b86
SamarasekaraHDK <it18063288@my.sliit.lk>
2021-10-02 23:23:07 +0530

    Initial commit

---
+++ src/main/java/com/ssd/ssd_assignment_02_v1/SsdAssignment02V1Application.java
@@ -0,0 +1,13 @@
+package com.ssd.ssd_assignment_02_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdAssignment02V1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsdAssignment02V1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssd/ssd_assignment_02_v1/SsdAssignment02V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd.ssd_assignment_02_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdAssignment02V1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
